2dda4911db665d4bee5431fbdaf60c3dc844ddae
nicetryharshit <harshitdubeyhd@gmail.com>
2022-10-05 14:35:38 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Understand Emojis
+An app that shows the meaning of the emoji.
+> this is the project for the markEight
+
+- When on a emoji clicked from the panel, its meaning will be shown.
+- You can also enter any emoji in the text box above to know its meaning.
+- An error message will be shown when the emoji is not present in the database.
+- This app was build using ReactJS.
--- src/App.js
+++ src/App.js
@@ -1,59 +1,25 @@
-import { useState } from "react";
-import "./styles.css";
+import logo from './logo.svg';
+import './App.css';
 
-var emojiDictionary = {
-  "🙂": "Smile",
-  "😆": "Laugh",
-  "😒": "Unamused",
-  "🥵": "Hot",
-  "🥶": "Cold"
-};
-
-export default function App() {
-  //[varToUpdate, functionTheVarRespondsTo] = useState("defaultValue")
-  const [emojiMeaning, setEmojiMeaning] = useState("");
-
-  function onEmojiClick(emoji) {
-    var meaning = emojiDictionary[emoji];
-    setEmojiMeaning(meaning);
-  }
-
-  function onEmojiInputChange(event) {
-    var userInput = event.target.value;
-    var meaning = emojiDictionary[userInput];
-
-    if (userInput !== "") {
-      if (meaning === undefined) {
-        meaning = "Meaning not found in our database";
-      }
-    } else {
-      meaning = "";
-    }
-    setEmojiMeaning(meaning);
-  }
-
+function App() {
   return (
     <div className="App">
-      <h1> Inside out </h1>
-      <input onChange={onEmojiInputChange}></input>
-      <h2>{emojiMeaning}</h2>
-      <h3>Our emoji database</h3>
-      {Object.keys(emojiDictionary).map(function (item) {
-        return (
-          <span
-            onClick={() => onEmojiClick(item)}
-            className="emoji-list"
-            key={item}
-          >
-            {item}
-          </span>
-        );
-      })}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-//Notes:
-
-//onclick use arrow function to prevent too many render errors?
-//https://stackoverflow.com/questions/59304283/error-too-many-re-renders-react-limits-the-number-of-renders-to-prevent-an-in
+export default App;
